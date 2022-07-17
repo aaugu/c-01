041864549afb0e0a3e45bb8986e8d4ff91beb812
Anais Augu <augu.anais@gmail.com>
2022-07-17 23:58:43 +0200

    ex01

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -1,10 +1,11 @@
+/*
 #include <stdio.h>
-
+*/
 void    ft_ft(int *nbr)
 {
     *nbr = 42;
 }
-
+/*
 int main(void)
 {
     int nbr;
@@ -13,3 +14,4 @@ int main(void)
     ft_ft(&nbr);
     printf("%d", nbr);
 }
+*/
